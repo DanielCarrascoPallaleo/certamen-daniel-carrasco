fac3eced683ce4d96068aadbe1e2296f78c4fb22
DanielCarrascoPallaleo <carrascopallaleo.daniel@gmail.com>
2025-11-07 13:04:07 -0400

    configuración de mis dependencias instaladas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Imporo el tema de PrimeReact
+import 'primereact/resources/themes/saga-blue/theme.css'; 
+// Importo estilos base de PrimeReact
+import 'primereact/resources/primereact.min.css';
+// Importa los íconos
+import 'primeicons/primeicons.css';
+// Importo Bootstrap
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+// Importo un css custom
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
